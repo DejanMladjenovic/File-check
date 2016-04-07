efa767f3b07ea30d3ddf43ff1b71e490fadc17c4
Ana Jozic Agic <ana.jozic.agic@kolektiv.eu>
2016-04-07 12:55:08 +0200

    Do mene je bilo

--- src/filecheck/Filecheck.java
+++ src/filecheck/Filecheck.java
@@ -57,6 +57,11 @@ public class Filecheck {
 		//Broj recenica u fajlu
 		System.out.println("Broj recenica: " + fajl.sentences(list));
 		
+		//Broj slova u fajlu
+		System.out.println("Broj slova u textu: ");
+		for (char i = 'a'; i <= 'z'; i++) {
+			System.out.println(i + " = " + (int) (fajl.brojSlova(list)[i]));
+		}
 	}
 	
 }
